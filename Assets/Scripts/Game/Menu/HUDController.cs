ebd5aa2a82899f3f17782965bca7c807607eca23
Håvard Aasmo <havi95@gmail.com>
2017-12-14 13:43:26 +0100

    Add mini profiler
    You can now press F1 while playing to toggle a profiler that shows FPS
    and memory usage.

---
+++ Assets/Scripts/Game/Menu/HUDController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour {
+
+    public GameObject profilerPanel;
+    private bool showProfiler = false;
+
+    void Awake() {
+        profilerPanel.SetActive(showProfiler);
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.F1)) {
+            showProfiler = !showProfiler;
+            ShowProfiler(showProfiler);
+        }
+
+    }
+
+    public void ShowProfiler(bool show) {
+        profilerPanel.SetActive(show);
+        showProfiler = show;
+    }
+
+}
