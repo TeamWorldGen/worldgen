1679d5d6dc9641cff5810daaf76a27b2b6816cf5
Mr_Oliver <Oliver.Nakken@Hotmail.com>
2018-01-07 17:32:56 +0100

    shaders with windmap proof of concept

---
+++ assets/Shaders/shader_informants/grass_informant.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class grass_informant : MonoBehaviour {
+	private float originy;
+
+	// Use this for initialization
+	void Start () {
+		originy = transform.eulerAngles.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Renderer>().material.SetFloat("originy", originy);
+
+		
+	}
+}
